5ebb195e4f8b8f1ebfbd1dad9d6d736d65e004d8
Wrede <WrJo@saracus.com>
2023-01-30 11:58:18 +0100

    changed package name

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="streamlit_cookies_manager",
     version="1.0",
-    packages=['streamlit_cookies_manager']
+    packages=['streamlit_cookies_manager'],
+    package_data={},
+    include_package_data=True
 )
