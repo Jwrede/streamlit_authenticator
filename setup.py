1fa59b085c75c11527b18b24e024d9b0fe2461a0
Wrede <WrJo@saracus.com>
2023-01-30 12:20:32 +0100

    changed folder structure

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 
 setup(
     name="streamlit_cookies_manager",
     version="1.0",
-    packages=find_packages(),
+    packages=['streamlit_cookies_manager'],
     package_data={},
     include_package_data=True
 )
